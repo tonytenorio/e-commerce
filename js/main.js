e50fbf314eb8fd6565c78f1a54b1d1493e425525
tony <tonytenorioc@gmail.com>
2019-05-05 12:23:34 -0600

    Header Section completed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    $("#btnCategorias").click(function(){
+        if(window.matchMedia("(max-width:767px)").matches){
+
+            $("#btnCategorias").after($("#categorias").slideToggle("fast"))
+
+        }else{
+            $("#cabezote").after($("#categorias").slideToggle("fast"))   
+        }
+    });
+})
