509feae534aad7e845a2e9164118d4fe205bdc84
tony <tonytenorioc@gmail.com>
2019-05-20 12:08:50 -0600

    Project completed all the sections ready

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    // Herramienta TOOLTIP
+    $('[data-toggle="tooltip"]').tooltip(); 
+
     var item = 0;
     var itemPaginacion = $("#paginacion li");
     var interrumpirCiclo = false;
@@ -208,6 +211,69 @@ $(document).ready(function(){
 
     })
 
+    /*=============================================
+    CUADRÍCULA O LISTA
+    =============================================*/
 
+    var btnList = $(".btnList");
+
+    for(var i = 0; i < btnList.length; i++){
+
+        $("#btnGrid"+i).click(function(){
+
+            var numero = $(this).attr("id").substr(-1);
+
+            $(".list"+numero).hide();
+            $(".grid"+numero).show();
+
+            $("#btnGrid"+numero).addClass("backColor");
+            $("#btnList"+numero).removeClass("backColor");
+
+        })
+
+        $("#btnList"+i).click(function(){
+
+            var numero = $(this).attr("id").substr(-1);
+
+            $(".list"+numero).show();
+            $(".grid"+numero).hide();
+
+            $("#btnGrid"+numero).removeClass("backColor");
+            $("#btnList"+numero).addClass("backColor");
+
+        })
+
+    }
+
+    /*=============================================
+    EFECTOS CON EL SCROLL
+    =============================================*/
+
+    $(window).scroll(function(){
+
+        var scrollY = window.pageYOffset;
+
+        if(window.matchMedia("(min-width:768px)").matches){
+
+            if(scrollY < ($(".banner").offset().top)-150){
+
+                $(".banner img").css({"margin-top":-scrollY/3+"px"})
+
+            }else{
+
+                scrollY = 0;
+            }
+
+        }	
+        
+    })
+
+    $.scrollUp({
+
+        scrollText:"",
+        scrollSpeed: 2000,
+        easingType: "easeOutQuint"
+
+    });
     
 })
